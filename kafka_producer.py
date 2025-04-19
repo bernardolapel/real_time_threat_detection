20f5e50341745fade80e85ee751dc9c0aedc9a3b
bernardolapel <114789731+bernardolapel@users.noreply.github.com>
2025-04-18 21:41:20 -0600

    Update kafka_producer.py

--- kafka_producer.py
+++ kafka_producer.py
@@ -1,15 +1,27 @@
 from kafka import KafkaProducer
 import json
 import time
+import requests
 
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
-with open('sample_logs.json') as f:
-    logs = json.load(f)
+url = "https://malicious-scanner.p.rapidapi.com/rapid/url"
+querystring = {"url": "https://vryjm.page.link/jS6a"}
 
-for log in logs:
-    producer.send('network-logs', log)
+headers = {
+    "x-rapidapi-key": "ba2dada015msh3dee5ece6fdb97fp128156jsn811fe85679fa",
+    "x-rapidapi-host": "malicious-scanner.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+#print(response.json())
+
+with open('response.json') as f:
+    res = json.load(f)
+
+for r in res:
+    producer.send('mal_response', r)
     time.sleep(1)
